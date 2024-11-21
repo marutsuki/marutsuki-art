62df67fc1a11542743c0dbe69e3fc3eefcb2489e
Lucien Lu <lucien.7789@gmail.com>
2024-11-21 20:48:16 +1100

    More Art dump, added year labels

--- src/layouts/ArtBoard.tsx
+++ src/layouts/ArtBoard.tsx
@@ -1,11 +1,15 @@
 import { FC, ReactNode } from "react";
 
 export type ArtBoardProps = {
+  label?: string;
   children?: ReactNode | ReactNode[];
 };
 
-const ArtBoard: FC<ArtBoardProps> = ({ children }) => (
-  <section className="grid grid-cols-3 gap-3">{children}</section>
+const ArtBoard: FC<ArtBoardProps> = ({ label, children }) => (
+  <section className="flex flex-row gap-12">
+    <h2 className="text-2xl">{label}</h2>
+    <div className="grid grid-cols-3 gap-3">{children}</div>
+  </section>
 );
 
 export default ArtBoard;
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -4,7 +4,11 @@ import ArtBoard from "./ArtBoard";
 
 const Main: FC = () => (
   <main>
-    <ArtBoard>
+    <ArtBoard label="2024">
+      <Artwork src="/art/21.png" />
+      <Artwork src="/art/22.png" />
+    </ArtBoard>
+    <ArtBoard label="2023">
       <Artwork src="/art/1.png" />
       <Artwork src="/art/2.png" />
       <Artwork src="/art/3.png" />
@@ -13,6 +17,8 @@ const Main: FC = () => (
       <Artwork src="/art/6.png" />
       <Artwork src="/art/7.png" />
       <Artwork src="/art/8.png" />
+    </ArtBoard>
+    <ArtBoard label="2022">
       <Artwork src="/art/9.png" />
       <Artwork src="/art/10.png" />
       <Artwork src="/art/11.png" />
@@ -20,6 +26,19 @@ const Main: FC = () => (
       <Artwork src="/art/13.png" />
       <Artwork src="/art/14.png" />
       <Artwork src="/art/15.png" />
+      <Artwork src="/art/16.png" />
+      <Artwork src="/art/17.png" />
+      <Artwork src="/art/18.png" />
+      <Artwork src="/art/19.png" />
+      <Artwork src="/art/20.png" />
+    </ArtBoard>
+    <ArtBoard label="2021">
+      <Artwork src="/art/23.png" />
+      <Artwork src="/art/24.png" />
+      <Artwork src="/art/25.png" />
+      <Artwork src="/art/26.png" />
+      <Artwork src="/art/27.png" />
+      <Artwork src="/art/28.png" />
     </ArtBoard>
   </main>
 );
